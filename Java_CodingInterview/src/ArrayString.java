85b41c1b3c956196df1f0b911eaebacee33b1312
cslongjian <504768527@qq.com>
2016-02-22 23:45:05 +0800

    配置Java代码

---
+++ Java_CodingInterview/src/ArrayString.java
@@ -0,0 +1,12 @@
+
+public class ArrayString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("----------启动Java版本的编码----------");
+	}
+
+}
